6d7fd3927a8b0e701cc67ec92613f2a34a2ebced
mkchang <6880917+mkchang@users.noreply.github.com>
2020-11-29 16:37:10 -0800

    Add local testing https server

---
+++ index.js
@@ -0,0 +1,90 @@
+const https = require('https');
+const fs = require('fs');
+const key = fs.readFileSync('./key.pem');
+const cert = fs.readFileSync('./cert.pem');
+const express = require('express');
+const path = require('path');
+
+const debug = require('debug')('mkchang.github.io:server');
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const HTTPS_PORT = 3001;
+const HTTP_PORT = 3000;
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(HTTP_PORT, () => console.log(`listening on ${HTTPS_PORT} for https, ${HTTP_PORT} for http`));
+const server = https.createServer({key, cert}, app);
+server.listen(HTTPS_PORT);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+module.exports = app;
---
+++ public/js/main.js
@@ -0,0 +1,46 @@
+window.onload = async function() {
+    const state = {
+        webGazerOn: false
+    };
+    const webGazerStart = document.getElementById('WebGazerStart');
+    const webGazerStop = document.getElementById('WebGazerStop');
+
+    webGazerStart.addEventListener('click', async () => {
+        if (!state.webGazerOn) {
+            webgazer.setGazeListener(function(data, elapsedTime) {
+                // if (data == null) {
+                //     return;
+                // }
+            }).begin()
+            .showPredictionPoints(true);
+            state.webGazerOn = true;
+        }
+    });
+
+    webGazerStop.addEventListener('click', () => {
+        if (state.webGazerOn) {
+            webgazer.pause();
+            webgazer.stopVideo();
+            webgazer.showPredictionPoints(false);
+            webgazer.end();
+            state.webGazerOn = false;
+        }
+    });
+};
+
+// Set to true if you want to save the data even if you reload the page.
+window.saveDataAcrossSessions = true;
+
+window.onbeforeunload = function() {
+    webgazer.end();
+}
+
+/**
+ * Restart the calibration process by clearing the local storage and reseting the calibration point
+ */
+function Restart(){
+    document.getElementById("Accuracy").innerHTML = "<a>Not yet Calibrated</a>";
+    webgazer.clearData();
+    ClearCalibration();
+    PopUpInstruction();
+}
