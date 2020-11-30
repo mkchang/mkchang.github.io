46f3c88727dc0bffca808507a8c97215047c5365
mkchang <6880917+mkchang@users.noreply.github.com>
2020-11-29 18:43:19 -0800

    Add calibration feature and docs

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,20 @@
+const state = {
+    webGazerOn: false
+};
+
 window.onload = async function() {
-    const state = {
-        webGazerOn: false
-    };
+
     const webGazerStart = document.getElementById('WebGazerStart');
     const webGazerStop = document.getElementById('WebGazerStop');
+    const webGazerRecalibrate = document.getElementById('WebGazerRecalibrate');
 
-    webGazerStart.addEventListener('click', async () => {
-        if (!state.webGazerOn) {
-            webgazer.setGazeListener(function(data, elapsedTime) {
-                // if (data == null) {
-                //     return;
-                // }
-            }).begin();
-            webgazer.showPredictionPoints(true);
-            state.webGazerOn = true;
-        }
-    });
+    webGazerStart.addEventListener('click', startWebGazer);
 
-    webGazerStop.addEventListener('click', () => {
-        if (state.webGazerOn) {
-            webgazer.pause();
-            webgazer.stopVideo();
-            webgazer.showPredictionPoints(false);
-            var gazeDot = document.getElementById('webgazerGazeDot');
-            gazeDot && gazeDot.remove();
-            webgazer.end();
-            state.webGazerOn = false;
-        }
+    webGazerStop.addEventListener('click', stopWebGazer);
+
+    webGazerRecalibrate.addEventListener('click', () => {
+        stopWebGazer();
+        Recalibrate();
     });
 };
 
@@ -37,11 +25,35 @@ window.onbeforeunload = function() {
     webgazer.end();
 }
 
+function startWebGazer() {
+    if (!state.webGazerOn) {
+        webgazer.setGazeListener(function(data, elapsedTime) {
+            // if (data == null) {
+            //     return;
+            // }
+        }).begin();
+        webgazer.showPredictionPoints(true);
+        state.webGazerOn = true;
+    }
+}
+
+function stopWebGazer() {
+        if (state.webGazerOn) {
+            webgazer.pause();
+            webgazer.stopVideo();
+            webgazer.showPredictionPoints(false);
+            var gazeDot = document.getElementById('webgazerGazeDot');
+            gazeDot && gazeDot.remove();
+            webgazer.end();
+            state.webGazerOn = false;
+        }
+    }
+
 /**
  * Restart the calibration process by clearing the local storage and reseting the calibration point
  */
-function Restart(){
-    document.getElementById("Accuracy").innerHTML = "<a>Not yet Calibrated</a>";
+function Recalibrate(){
+    // document.getElementById("Accuracy").innerHTML = "<a>Not yet Calibrated</a>";
     webgazer.clearData();
     ClearCalibration();
     PopUpInstruction();
