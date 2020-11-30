3cf957107af85e1a3cf2861d1f4c31ff6ce1596c
mkchang <6880917+mkchang@users.noreply.github.com>
2020-11-29 21:16:00 -0800

    Add active highlighting of target tile

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,15 @@
 const state = {
     webGazerOn: false
 };
+let yesBtn, noBtn;
 
 window.onload = async function() {
 
     const webGazerStart = document.getElementById('WebGazerStart');
     const webGazerStop = document.getElementById('WebGazerStop');
     const webGazerRecalibrate = document.getElementById('WebGazerRecalibrate');
+    yesBtn = document.querySelector('.Tile.Yes');
+    noBtn = document.querySelector('.Tile.No');
 
     webGazerStart.addEventListener('click', startWebGazer);
 
@@ -28,9 +31,13 @@ window.onbeforeunload = function() {
 function startWebGazer() {
     if (!state.webGazerOn) {
         webgazer.setGazeListener(function(data, elapsedTime) {
-            // if (data == null) {
-            //     return;
-            // }
+            if (data == null) {
+                return;
+            }
+            const xprediction = data.x; //these x coordinates are relative to the viewport
+            const yprediction = data.y; //these y coordinates are relative to the viewport
+            const targetEl = document.elementFromPoint(xprediction, yprediction);
+            highlightActiveTileThrottled(targetEl);
         }).begin();
         webgazer.showPredictionPoints(true);
         state.webGazerOn = true;
@@ -48,6 +55,46 @@ function stopWebGazer() {
             state.webGazerOn = false;
         }
     }
+
+function justThrottle(fn, interval, callFirst) {
+    var wait = false;
+    var callNow = false;
+    return function() {
+        callNow = callFirst && !wait;
+        var context = this;
+        var args = arguments;
+        if (!wait) {
+            wait = true;
+            setTimeout(function() {
+                wait = false;
+                if (!callFirst) {
+                    return fn.apply(context, args);
+                }
+            }, interval);
+        }
+        if (callNow) {
+            callNow = false;
+            return fn.apply(this, arguments);
+        }
+    };
+}
+
+function highlightActiveTile(targetEl) {
+    yesBtn = yesBtn || document.querySelector('.Tile.Yes');
+    noBtn = noBtn || document.querySelector('.Tile.No');
+    if ($.contains(yesBtn, targetEl) || targetEl === yesBtn) {
+        noBtn.classList.remove('active');
+        yesBtn.classList.add('active');
+    } else if ($.contains(noBtn, targetEl) || targetEl === noBtn) {
+        yesBtn.classList.remove('active');
+        noBtn.classList.add('active');
+    } else {
+        yesBtn.classList.remove('active');
+        noBtn.classList.remove('active');
+    }
+}
+
+const highlightActiveTileThrottled = justThrottle(highlightActiveTile, 500);
 
 /**
  * Restart the calibration process by clearing the local storage and reseting the calibration point
